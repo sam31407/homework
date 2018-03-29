3b9b6f6a1de23408f95401dd03eb65a051927298
user <user@MA324-18>
2018-03-29 14:36:34 +0800

    hw

---
+++ homework/Controllers/hwController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace homework.Controllers
+{
+    public class hwController : Controller
+    {
+        // GET: hw
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
